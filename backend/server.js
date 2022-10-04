87bdc37cc7a4572a5fd3ecadbf9a3ebff067264a
brunonasc74 <bruno.hpn74@gmail.com>
2022-10-04 06:12:10 -0300

    basic express server setup

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,9 @@
-console.log('hello world');
+import express from 'express';
+import dotenv from 'dotenv';
+
+const app = express();
+app.use(express.json());
+
+const port = process.env.PORT || 4000;
+
+app.listen(port, () => console.log(`running on port ${port}`));
