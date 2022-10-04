a2e99f1d85fdbf7f91206fb093e7636dac0df882
brunonasc74 <bruno.hpn74@gmail.com>
2022-10-04 06:32:29 -0300

    code refactored and first route created

--- backend/index.js
+++ backend/index.js
@@ -4,6 +4,10 @@ import dotenv from 'dotenv';
 const app = express();
 app.use(express.json());
 
+app.get('/api/goals', (_, res) => {
+	res.send({ message: 'Get Goals' });
+});
+
 const port = process.env.PORT || 4000;
 
-app.listen(port, () => console.log(`running on port ${port}`));
+app.listen(port, () => console.log(`Server running on port ${port}`));
