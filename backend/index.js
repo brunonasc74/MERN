7d1553faac414557ee7ee84ec4461fa4848e5312
brunonasc74 <bruno.hpn74@gmail.com>
2022-10-05 21:43:48 -0300

    middleware for urlencoded added

--- backend/index.js
+++ backend/index.js
@@ -3,6 +3,7 @@ import dotenv from 'dotenv';
 
 const app = express();
 app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
 app.get('/api/goals', (_, res) => {
 	res.send({ message: 'Get Goals' });
