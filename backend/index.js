75380f6afcb46d3070182a4c87f2ff973b495d1d
brunonasc74 <bruno.hpn74@gmail.com>
2022-10-10 09:56:03 -0300

    removed test get method in index

--- backend/index.js
+++ backend/index.js
@@ -12,10 +12,6 @@ app.use(express.urlencoded({ extended: false }));
 
 router(app);
 
-app.get('/api/goals', (_, res) => {
-	res.send({ message: 'Get Goals' });
-});
-
 const port = process.env.PORT || 4000;
 
 app.listen(port, () => console.log(`Server running on port ${port}`));
