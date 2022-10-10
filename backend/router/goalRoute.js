820a08ffcf26ae86fbd1d1d89320e3de55643f7d
brunonasc74 <bruno.hpn74@gmail.com>
2022-10-10 04:24:29 -0300

    goal route protected and relationship with user implemented

--- backend/router/goalRoute.js
+++ backend/router/goalRoute.js
@@ -1,11 +1,12 @@
 import GoalController from '../controllers/GoalController.js';
+import protect from '../middleware/authMiddleware.js';
 import { Router } from 'express';
 const router = Router();
 
 router
-	.get('/api/goals', GoalController.getGoals)
-	.post('/api/goals', GoalController.postGoals)
-	.put('/api/goals/:id', GoalController.editGoals)
-	.delete('/api/goals/:id', GoalController.deleteGoals);
+	.get('/api/goals', protect, GoalController.getGoals)
+	.post('/api/goals', protect, GoalController.postGoals)
+	.put('/api/goals/:id', protect, GoalController.editGoals)
+	.delete('/api/goals/:id', protect, GoalController.deleteGoals);
 
 export default router;
