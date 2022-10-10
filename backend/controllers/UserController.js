6ea9cb2489ed8cc3c40d86845c76cfa7a5719559
brunonasc74 <bruno.hpn74@gmail.com>
2022-10-09 23:44:22 -0300

    post method of register user created

--- backend/controllers/UserController.js
+++ backend/controllers/UserController.js
@@ -1,8 +1,35 @@
+import jwt from 'jsonwebtoken';
+import bcrypt from 'bcryptjs';
 import User from '../models/userModel.js';
 
 class UserController {
 	static async registerUser(req, res) {
-		res.send('register user');
+		try {
+			const { name, email, password } = req.body;
+			if (!name || !email || !password)
+				return res.status(400).send({ error: 'please enter all fields' });
+
+			const userExists = await User.findOne({ email });
+			if (userExists)
+				return res.status(400).send({ error: 'user already exists' });
+
+			// Hash password
+			const salt = await bcrypt.genSalt(10);
+			const hashedPassword = await bcrypt.hash(password, salt);
+
+			// Create user
+			const user = await User.create({
+				name,
+				email,
+				password: hashedPassword
+			});
+			if (!user) return res.status(400).send({ error: 'invalid user data' });
+			res
+				.status(201)
+				.send({ _id: user.id, name: user.name, email: user.email });
+		} catch (err) {
+			res.status(400).send(err);
+		}
 	}
 
 	static async loginUser(req, res) {
